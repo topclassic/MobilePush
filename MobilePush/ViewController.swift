15ec02447774bc40e8ff001fe96b5e331c6f2eed
Chotipat Top <topz.classic@gmail.com>
2016-07-01 11:06:07 +0700

    Initial Commit

---
+++ MobilePush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobilePush
+//
+//  Created by Digital-MacbookPro on 7/1/2559 BE.
+//  Copyright © 2559 Digital-MacbookPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
